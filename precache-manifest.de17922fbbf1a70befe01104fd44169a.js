58579372910ec6e5bc7135ffc9822832966402e3
kaustubh k patil <kaustubhpatil@kaustubhs-MacBook-Pro.local>
2022-03-26 14:23:37 +0530

    Updates

---
+++ precache-manifest.de17922fbbf1a70befe01104fd44169a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf72ffa8285f8a096a91eb6f856079bb",
+    "url": "/SuhasyaDentalClinic/index.html"
+  },
+  {
+    "revision": "08cf6681424d723e0914",
+    "url": "/SuhasyaDentalClinic/static/css/2.7288dd81.chunk.css"
+  },
+  {
+    "revision": "f33ac0af52d09b5e97e9",
+    "url": "/SuhasyaDentalClinic/static/css/main.e5291d5b.chunk.css"
+  },
+  {
+    "revision": "08cf6681424d723e0914",
+    "url": "/SuhasyaDentalClinic/static/js/2.0e7e31db.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/SuhasyaDentalClinic/static/js/2.0e7e31db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f33ac0af52d09b5e97e9",
+    "url": "/SuhasyaDentalClinic/static/js/main.7939673e.chunk.js"
+  },
+  {
+    "revision": "2d843419a391e516b7e3",
+    "url": "/SuhasyaDentalClinic/static/js/runtime-main.16de97b1.js"
+  },
+  {
+    "revision": "4d72a82c5f0f51f915148f64392c2d06",
+    "url": "/SuhasyaDentalClinic/static/media/Cosmetic_Dentistry.4d72a82c.png"
+  },
+  {
+    "revision": "29ff9f1a69a667d15f8fbafd70142a20",
+    "url": "/SuhasyaDentalClinic/static/media/Dental_Care_Service.29ff9f1a.png"
+  },
+  {
+    "revision": "d0295a2de5ed2bc30c4dcb6da7e35dbe",
+    "url": "/SuhasyaDentalClinic/static/media/Emergency_Care_Service.d0295a2d.png"
+  },
+  {
+    "revision": "9983b00a80a960e7720fda0a821585f2",
+    "url": "/SuhasyaDentalClinic/static/media/Restoration_Dentistry.9983b00a.png"
+  },
+  {
+    "revision": "dde6943dce35c9b3476d7a84cff01263",
+    "url": "/SuhasyaDentalClinic/static/media/Testimonials_Logo.dde6943d.png"
+  },
+  {
+    "revision": "e57ace8ba829317db1cb52f43ac4b1ec",
+    "url": "/SuhasyaDentalClinic/static/media/doctors.e57ace8b.png"
+  },
+  {
+    "revision": "2342c197ed64d3cdf388a8e1fff0f482",
+    "url": "/SuhasyaDentalClinic/static/media/navbar.2342c197.JPG"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/SuhasyaDentalClinic/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/SuhasyaDentalClinic/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/SuhasyaDentalClinic/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/SuhasyaDentalClinic/static/media/slick.f97e3bbf.svg"
+  }
+]);
